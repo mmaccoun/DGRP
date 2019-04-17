7db67c29ee1332c185d6494db38a44ee3bccd0fd
Mary Maccoun <40074843+mmaccoun@users.noreply.github.com>
2019-04-17 15:22:50 -0700

    Update data.R

--- data.R
+++ data.R
@@ -9,33 +9,78 @@
 library(lattice)
 library(ggplot2)
 library(caret)
+library(tidyr)
 
-setwd("~/Downloads/DGRP-master")
+set.seed(100)
+
+setwd("~/Desktop/DGRP")
 
 ##############      import and format data      ##############
 
 
 ## get female data 
-female = read.table("./dgrp.array.exp.female.txt", quote="\"", comment.char="", header = FALSE)
-female = t(female) #row is a sample & one row is sex, [,1] is all the samples, sex is a column = 0 but [,2:] is genes
-female[,1] = 0 #change sample names to all 0 for ID as female... now [,1] is all 0 and rest are genes 
+female = read.table("./dgrp.array.exp.female.txt", quote="\"", comment.char="", header=T,
+                    stringsAsFactors= F)
+tmp = read.table("./dgrp.array.exp.female.txt", quote="\"", comment.char="", header=F,
+                    stringsAsFactors= F)
+tmp = t(tmp)
+female = t(female) #row is a sample & one row is sex, [,1] is all the samples,
+#                   sex is a column = 0 but [,2:] is genes
+tmp = as.data.frame(tmp) #matrix to data frame
+female = as.data.frame(female)
 
-female = as.data.frame(female) #values as integers 
-dim(female) #368 sampes, 18140 genes measured  
-print(female[1:5,1:5])
+#toSplit = data.frame(tmp[2:369,1])
 
-#print(female[1:5, 1:5])
+###LOOK LATER RUN LATER #########################
+colnames(tmp) = tmp[ ]
+female[1:10,1:10]
+#female = female[-c(1),]  #remove row 
+female[1:5,1:5]
+dim(female)
+################################################
+
+female$sex = "female"
+dim(tmp)
+female$ID = tmp[,1]
+tmp[1:5, 1:5]
+dim(tmp)
+tmp[1,1] = "null_null:null"
+tmp = separate(tmp, col = 1 , c("strain", "rep"), sep = ":") #C stack usage 76776039 is too close to the limit, must find alt way for data
+female[1:5,1:5]
+female$strain = tmp$strain 
+female$rep = tmp$rep
+
+female[-c(1)]
+female[1:5, 18140:18144]
+
 
 ## get male data 
-male = read.table("./dgrp.array.exp.male.txt", quote="\"", comment.char="", header = FALSE)
+male = read.table("./dgrp.array.exp.male.txt", quote="\"", comment.char="", header = T)
 male = t(male) #row is a sample & one row is sex, [,1] is all the samples, sex is a column = 0 but [,2:] is genes
-male[,1] = 1
+tmpM = read.table("./dgrp.array.exp.male.txt", quote="\"", comment.char="", header = F) 
+tmpM = t(tmpM)
+tmpM = as.data.frame(tmpM) #matrix to data frame
 male = as.data.frame(male)
-print(male[1:5,1:5])
-dim(male) #369 samples, 18140 genes measured 
 
-
+###split up data 
+male$sex = "male"
+dim(tmpM)
+dim(male)
+male[1:5,1:5]
+tmpM[1:5,1:5]
+male$ID = tmpM[,1]
+tmpM[1,1] = "null_null:null"
+tmpM = separate(tmpM, col = 1 , c("strain", "rep"), sep = ":") #C stack usage 76776039 is too close to the limit, must find alt way for data
+male[1:5,1:5]
+male$strain = tmpM$strain 
+male$rep = tmpM$rep
+male[-c(1)]
+male[1:5, 18140:18144]
 ## combine to one dataframe 
-final = rbind(female,male[2:370,]) #dont include male[1, ] header info from male
-write.csv(final, file = "all_data.csv")
+dim(male)
+dim(female)
+dim(final)
+final = rbind(female, male) #dont include male[1, ] header info from male
+write.csv(final, "./final.csv")
+
 
